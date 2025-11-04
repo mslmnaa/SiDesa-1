6c925d2d55306bce7412fc3c957b5101869e8572
Salman <149822719+mslmnaa@users.noreply.github.com>
2025-11-04 21:09:36 +0700

    a

--- database/seeders/LandingContentSeeder.php
+++ database/seeders/LandingContentSeeder.php
@@ -11,7 +11,7 @@ class LandingContentSeeder extends Seeder
 {
     public function run(): void
     {
-        // Clear existing data
+        // Clear existing dat
         // LandingContent::truncate();
         
         // Create storage directory if not exists
